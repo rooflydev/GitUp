a6a0e5e5be3f4a203733868f09b5cb29914b9b8b
roofly <roofly.300iq@gmail.com>
2026-02-08 22:34:45 +0100

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,25 @@
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
+      authorization: { params: { scope: 'identify email' } },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-});
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/signin', // Optionnel : si tu veux une page perso plus tard
+    error: '/auth/error', 
+  },
+};
+
+export default NextAuth(authOptions);
